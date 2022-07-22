eea82527ad3c9ffc25c18d0652f078829075c60c
mehedih23 <nmehedi23@gmail.com>
2022-07-22 23:48:36 +0600

    delete api done

--- index.js
+++ index.js
@@ -81,6 +81,17 @@ async function run() {
             const result = await itemCollection.updateOne(filter, updatedDoc)
             res.send(result);
         })
+
+
+
+
+        //------------------ Delete ----------------//
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
